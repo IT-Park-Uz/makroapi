fc9dab7746c04236a95ac1013b04d363204150b8
baxtikdev <ergashevb111@gmail.com>
2023-09-14 12:49:23 +0500

    cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -325,8 +325,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.OrderingFilter",
-        "rest_framework.filters.SearchFilter",
+        # "rest_framework.filters.OrderingFilter",
+        # "rest_framework.filters.SearchFilter",
     ),
 
     "DEFAULT_PERMISSION_CLASSES": (
