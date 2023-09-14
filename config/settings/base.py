67d7ae4123628b259c0667613ac65b64ede4597f
baxtikdev <ergashevb111@gmail.com>
2023-09-14 11:04:03 +0500

    static

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,13 +156,13 @@ MIDDLEWARE = [
 # ------------------------------------------------------------------------------
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(APPS_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # MEDIA
 # ------------------------------------------------------------------------------
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(APPS_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
