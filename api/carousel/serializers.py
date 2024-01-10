70234867454a7d8333e1b58913023bb0dc83ec40
STQT <productgsm@gmail.com>
2024-01-10 13:22:43 +0500

    add photo to carousel item

--- api/carousel/serializers.py
+++ api/carousel/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from common.carousel.models import CarouselItem
 
 
-
 class CarouselItemSerializer(serializers.ModelSerializer):
+    news_photo = serializers.ImageField(source="news.photo_medium", allow_null=True)
+    discount_photo = serializers.ImageField(source="discount.photo_medium", allow_null=True)
+
     class Meta:
         model = CarouselItem
         fields = '__all__'
