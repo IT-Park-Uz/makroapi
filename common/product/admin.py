d8a1d830a67137c57daa85da89762a21cdabe126
STQT <productgsm@gmail.com>
2024-05-10 21:15:46 +0500

    fix

--- common/product/admin.py
+++ common/product/admin.py
@@ -24,7 +24,7 @@ class FileAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(TabbedTranslationAdmin):
     list_display = ['title', 'display_image', 'code', 'region', 'newPrice', 'oldPrice', 'endDate', 'status', 'order']
-    list_display_links = list_display[:-1]
+    list_display_links = list_display[:-2]
     list_filter = ['category', 'top_category']
     list_editable = ["status", "order"]
     search_fields = ['title', 'newPrice', 'oldPrice']
