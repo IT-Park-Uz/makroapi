0bbb5b1223250ce50db169fa73fe9a69b314bf2e
STQT <productgsm@gmail.com>
2024-09-12 01:23:52 +0500

    fix2

--- api/carousel/serializers.py
+++ api/carousel/serializers.py
@@ -9,8 +9,6 @@ class CarouselItemSerializer(serializers.ModelSerializer):
     discount_photo_mobile = serializers.ImageField(source="discount.photo_mobile", allow_null=True)
     news_photo_app = serializers.ImageField(source="news.photo_app", allow_null=True)
     discount_photo_app = serializers.ImageField(source="discount.photo_app", allow_null=True)
-    discount_url = serializers.URLField(source="discount.url", allow_null=True)
-    news_url = serializers.URLField(source="news.url", allow_null=True)
 
     class Meta:
         model = CarouselItem
